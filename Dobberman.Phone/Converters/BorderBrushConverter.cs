4dfeb716a596269d7fbefaaf84942fccef13bd0c
link.mattan <link.mattan@gmail.com>
2012-04-17 15:28:54 +0000

    adding color converter
    git-svn-id: http://dobberman.googlecode.com/svn/trunk@50 c3bbc38d-7444-5f59-4a46-9a20b019e4f8

---
+++ Dobberman.Phone/Converters/BorderBrushConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TAUP2C.Dobberman.Phone.Converters
+{
+    public class BorderBrushConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var mood = (string) value;
+            switch (mood)
+            {
+                case "positive":
+                    return "DodgerBlue";
+                case "negative":
+                    return "IndianRed";
+                case "concerned":
+                    return "Gold";
+                case null:
+                    return "Gold";
+            }
+            return "DodgerBlue";
+        }
+
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
