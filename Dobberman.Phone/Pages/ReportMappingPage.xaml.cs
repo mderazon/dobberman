e4e58add0c86999ed6aec71247b3b569cec9532d
olgazeli <olgazeli@gmail.com>
2012-04-24 15:06:48 +0000

    Added changes at ReportMappinpage, takes all reports with location from DB, and displayes them on map.
    git-svn-id: http://dobberman.googlecode.com/svn/trunk@55 c3bbc38d-7444-5f59-4a46-9a20b019e4f8

--- Dobberman.Phone/Pages/ReportMappingPage.xaml.cs
+++ Dobberman.Phone/Pages/ReportMappingPage.xaml.cs
@@ -21,162 +21,112 @@ using Microsoft.Phone.Shell;
 using System.Windows.Navigation;
 using System.Text;
 
+using TAUP2C.Dobberman.Phone.DobbermanService;
+using System.Globalization;
+
 
 namespace TAUP2C.Dobberman.Phone.Pages
 {
     public partial class ReportMappingPage : PhoneApplicationPage
     {
+        List<GeoCoordinate> GeoLocationList = new List<GeoCoordinate>();
+        List<Report> reportList = new List<Report>();
         MapLayer PushpinLayer = new MapLayer();
         private List<Pushpin> pins = new List<Pushpin>();
         GeoCoordinate coordinate = new GeoCoordinate();
+        GeoCoordinate geotry = new GeoCoordinate(32.549, 35.234);
+        //string geotrystring = geotry.ToString();
+      //  string geotrystring = System.String.Empty;
+       // geotrystring = geotry.ToString();
         double lat = 0, lon = 0;
+      
+        
         public ReportMappingPage()
         {
+           
             InitializeComponent();
+            googlemap.SetView(geotry, 10);
+            DobbermanServiceClient client = new DobbermanServiceClient();
+            client.GetAllReportsWithLocationCompleted += new EventHandler<GetAllReportsWithLocationCompletedEventArgs>(client_FindReportCompleted);
+            client.GetAllReportsWithLocationAsync(1);
+            //Loaded += new RoutedEventHandler(PivotPage1_Loaded);
+
         }
+        /*from DB*/
+        void client_FindReportCompleted(object sender, GetAllReportsWithLocationCompletedEventArgs e)
+        {
+            
+            foreach (Report r in e.Result.Cast<Report>())
+            {
+                reportList.Add(r);
+            }
+          
+        }
+
+        /*coordunate from string to Geocordinate*/
+        public GeoCoordinate ParseGeoCoordinate(string input)
+        {
+            // GeoCoordinate.ToString() uses InvariantCulture, so the doubles will use '.'
+            // for decimal placement, even in european environments
+            string[] parts = input.Split(',');
+
+            if (parts.Length != 2)
+            {
+                throw new ArgumentException("Invalid format");
+            }
+
+            double latitude = Double.Parse(parts[0], CultureInfo.InvariantCulture);
+            double longitude = Double.Parse(parts[1], CultureInfo.InvariantCulture);
+
+            return new GeoCoordinate(latitude, longitude);
+        }
+
+        /*create pushpin*/
         private Pushpin CreatePin(string description, GeoCoordinate location)
         {
             Pushpin pin = new Pushpin();
             pin.Location = location;
             pin.Opacity = 0.4;
             pin.Content = description;
-            // if (isHoldEvent)
-            //{
-            //  pin.Hold += new EventHandler<System.Windows.Input.GestureEventArgs>(pinRemove_Hold);
-            //}
             return pin;
         }
-
-        /*   private void googlemap_Hold(object sender, System.Windows.Input.GestureEventArgs e)
-           {
-               Point p = e.GetPosition(this.googlemap);
-               GeoCoordinate geo = new GeoCoordinate();
-               geo = googlemap.ViewportPointToLocation(p);
-               GetPinPoint(geo, true);
-           }
-
-           private void GetPinPoint(GeoCoordinate geo, bool isHoldEvent)
-           {
-               Pushpin pin = CreatePin("Loading...", geo, isHoldEvent);
-               googlemap.Children.Add(pin);
-               string searchLocation = geo.Latitude + "," + geo.Longitude;
-               Uri requestAddress = new Uri("https://maps.googleapis.com/maps/api/geocode/xml?latlng=" + searchLocation + "&sensor=true");
-               WebClient downloader = new WebClient();
-               downloader.DownloadStringCompleted += new DownloadStringCompletedEventHandler(Downloaded_Revese);
-               downloader.DownloadStringAsync(requestAddress, pin);
-           }
-           */
-        private int RandomNumber(int min, int max)
-        {
-            Random random = new Random();
-            return random.Next(min, max);
-        }
-        private void GestureListener_Tap(object sender, GestureEventArgs e)
-        {
-            int success = 7;
-            this.NavigationService.Navigate(new Uri("/Pages/Page1.xaml", UriKind.Relative));
-        }
+  
+       /*display all reports*/
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-            IGeoPositionWatcher<GeoCoordinate> watcher;
+            
+            /*buid the geolocationList*/
+            for (int i = 0; i < reportList.Count; i++) // Loop through List with for
+            {
+                GeoLocationList.Add((ParseGeoCoordinate(reportList[i].Location)));
+            }
 
-            watcher = new System.Device.Location.GeoCoordinateWatcher();
+            /*display pushpins*/
 
-            // Do not suppress prompt, and wait 1000 milliseconds to start.
-            watcher.TryStart(false, TimeSpan.FromMilliseconds(1000));
-            GeoCoordinate coord = watcher.Position.Location;
-
-            if (coord.IsUnknown != true)
+            for (int i = 0; i < GeoLocationList.Count; i++) // Loop through List with for
             {
 
-                //  Console.WriteLine("Lat: {0}, Long: {1}",
-                //      coord.Latitude,
-                //    coord.Longitude);
-               // coord.Latitude = 31.802;
-                //coord.Longitude = 34.848;
-
-                 GeoCoordinate coord1 = new GeoCoordinate();
-            List<GeoCoordinate> myList = new List<GeoCoordinate>();
-
-            myList.Add(new GeoCoordinate(31.101, 35.145));
-            myList.Add(new GeoCoordinate(31.838, 34.878));
-            myList.Add(new GeoCoordinate(32.549, 35.234));
-                coord1 = myList[RandomNumber(0, 2)];
- 
+                GeoCoordinate coord1 = new GeoCoordinate();
+                 coord1 = GeoLocationList[i];
 
                 Pushpin pin = new Pushpin();
                 pin.Location = coord1;
-
-                // googlemap.Children.Add(PushpinLayer);
-                /*    Image image = new Image(); 
-                    BitmapImage bmi = new BitmapImage(new Uri("L_tshirt_front.png", UriKind.Relative));
-                    //img.Source = bmi;
-               
-                    textBlock.Text = "My Pushpin";
-                    Grid grid = new Grid();
-                    grid.Children.Add(image);
-                    grid.Children.Add(textBlock); 
-                
-                   // image.Source = ("Images\pushpin.png");
-                    image.Width = 40;
-                    image.Height = 40;
-                //    PushpinLayer.AddChild(image,coord);
-                  
-                    //new Microsoft.Maps.MapControl.Location(42.658, -71.137),  
-            //PositionOrigin.Center);*/
-                watcher.Stop();
+                pin.Template = null; 
+                pin.Content = new Ellipse()
+                {
+                    Fill = new SolidColorBrush(Colors.Red),
+                    Stroke = new SolidColorBrush(Colors.Black),
+                    StrokeThickness = 4,
+                    Opacity = .8,
+                    Height = 25,
+                    Width = 25
+                };
                 googlemap.Children.Add(pin);
-                //   pin.DoubleTap = new EventHandler<System.Windows.Input.GestureEventArgs>(pin1_MouseLeftButtonUp);
-
-                var gl = GestureService.GetGestureListener(pin);
-                gl.Tap += new EventHandler<GestureEventArgs>(GestureListener_Tap); 
-
-            }
                 
-            else
-            {
-                print.Text = ("Unknown latitude and longitude");
-                //Console.WriteLine("Unknown latitude and longitude.");
+                googlemap.SetView(coord1, 7); //at the navigation     
             }
         }
-
-
     }
     }
 
     
-
-
-/*
-MapLayer m_PushpinLayer = new MapLayer();
-Your_Map.Children.Add(m_PushpinLayer);
-Image image = new Image();
-image.Source = ResourceFile.GetBitmap("Images/Me.png", From.This);
-image.Width = 40;
-image.Height = 40;
-m_PushpinLayer.AddChild(image,
-    new Microsoft.Maps.MapControl.Location(42.658, -71.137),  
-        PositionOrigin.Center);
-
-
-
- private void AddPinToPointsOfIntrests(Pushpin pin)
-        {
-            WayPoint point = new WayPoint();
-            point.Description = pin.Content.ToString();
-            point.LocationLat = pin.Location.Latitude;
-            point.LocationLng = pin.Location.Longitude;
-            this.view.Favorits.PointOfInterst.Add(point);
-        }
-
-        private Pushpin CreatePin(string description, GeoCoordinate location, bool isHoldEvent)
-        {
-            Pushpin pin = new Pushpin();
-            pin.Location = location;
-            pin.Opacity = 0.4;
-            pin.Content = description;
-            if (isHoldEvent)
-            {
-                pin.Hold += new EventHandler<System.Windows.Input.GestureEventArgs>(pinRemove_Hold);
-            }
-            return pin;*/
